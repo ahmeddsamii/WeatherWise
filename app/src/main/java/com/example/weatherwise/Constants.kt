bcca44b08be99de931bd4c05dd4ddbb1e2f62b1f
Ahmed Samy <ahmbdelkhalek@gmail.com>
2024-09-27 19:15:30 +0300

    added baseUrl and apiKey constants

--- app/src/main/java/com/example/weatherwise/Constants.kt
+++ app/src/main/java/com/example/weatherwise/Constants.kt
@@ -1,3 +1,7 @@
 package com.example.weatherwise
+object Constants{
+    val LOCATION_REQUEST_CODE = 1001
+    const val BASE_URL =  "https://api.openweathermap.org/data/2.5/"
+    const val API_KEY = "e9556a170d2136f283a92527fbc67b3d"
+}
 
-val LOCATION_REQUEST_CODE = 1001
