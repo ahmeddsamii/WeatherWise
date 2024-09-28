aff9ffc65ac5fd5ea428063682229dd0c3cc5803
Ahmed Samy <ahmbdelkhalek@gmail.com>
2024-09-28 17:10:36 +0300

    added weather repo

---
+++ app/src/main/java/com/example/weatherwise/repositories/WeatherRepository.kt
@@ -0,0 +1,27 @@
+import com.example.weatherwise.model.WeatherForecastResponse
+import com.example.weatherwise.network.api.RetrofitHelper
+import retrofit2.Response
+
+class WeatherRepository private constructor() {
+    private val apiService = RetrofitHelper.apiService
+
+   companion object{
+       private var INSTANCE: WeatherRepository? = null
+       fun getInstance():WeatherRepository{
+           return INSTANCE?: synchronized(this){
+               val instance = WeatherRepository()
+                INSTANCE=instance
+               instance
+           }
+       }
+   }
+
+
+    suspend fun getCurrentWeather(lat:Double, long:Double, apiKey:String): Response<WeatherResponse> {
+       return apiService.getCurrentWeather(lat,long,apiKey)
+    }
+
+    suspend fun getWeatherForecast(lat:Double, long:Double, apiKey:String): Response<WeatherForecastResponse>{
+        return apiService.getWeatherForecast(lat,long,apiKey)
+    }
+}
