a57fbc844202191726f2df41346f563895d7cf91
Ahmed Samy <ahmbdelkhalek@gmail.com>
2024-09-28 17:16:09 +0300

    displaying current weather and weather of every 3 hours of the day next five days

--- app/src/main/java/com/example/weatherwise/MainActivity.kt
+++ app/src/main/java/com/example/weatherwise/MainActivity.kt
@@ -1,7 +1,7 @@
 package com.example.weatherwise
 
+import android.content.pm.PackageManager
 import android.os.Bundle
-import android.view.Menu
 import androidx.activity.enableEdgeToEdge
 import com.google.android.material.navigation.NavigationView
 import androidx.navigation.findNavController
@@ -11,7 +11,9 @@ import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
 import androidx.drawerlayout.widget.DrawerLayout
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.fragment.NavHostFragment
 import com.example.weatherwise.databinding.ActivityMainBinding
+import com.example.weatherwise.ui.home.view.HomeFragment
 
 class MainActivity : AppCompatActivity() {
 
@@ -39,13 +41,47 @@ class MainActivity : AppCompatActivity() {
         navView.setupWithNavController(navController)
     }
 
-    override fun onCreateOptionsMenu(menu: Menu): Boolean {
-        menuInflater.inflate(R.menu.main, menu)
-        return true
-    }
+//    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+//        menuInflater.inflate(R.menu.main, menu)
+//        return true
+//    }
+//
+//
+//    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+//        val navController = findNavController(R.id.nav_host_fragment_content_main)
+//        return when (item.itemId) {
+//            R.id.nav_settings -> {
+//                navController.navigate(R.id.nav_settings)
+//                true
+//            }
+//            else -> super.onOptionsItemSelected(item)
+//        }
+//    }
+
 
     override fun onSupportNavigateUp(): Boolean {
         val navController = findNavController(R.id.nav_host_fragment_content_main)
         return navController.navigateUp(appBarConfiguration) || super.onSupportNavigateUp()
+    }
+
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if (requestCode == Constants.LOCATION_REQUEST_CODE) {
+            val isGranted = grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED
+
+            // Find the NavHostFragment
+            val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment_content_main) as? NavHostFragment
+
+            // Find the HomeFragment within the NavHostFragment
+            val homeFragment = navHostFragment?.childFragmentManager?.fragments?.find { it is HomeFragment } as? HomeFragment
+
+            // Call the method on HomeFragment
+            homeFragment?.onPermissionResult(isGranted)
+        }
     }
 }
