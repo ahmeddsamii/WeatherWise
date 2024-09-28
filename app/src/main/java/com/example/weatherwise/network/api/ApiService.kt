84232dab57411ef4d2c8fe1223db3562c09af20e
Ahmed Samy <ahmbdelkhalek@gmail.com>
2024-09-28 19:46:29 +0300

    added query for C instead of kelvien

--- app/src/main/java/com/example/weatherwise/network/api/ApiService.kt
+++ app/src/main/java/com/example/weatherwise/network/api/ApiService.kt
@@ -8,11 +8,19 @@ import retrofit2.http.Query
 
 interface ApiService {
     @GET("weather")
-    suspend fun getCurrentWeather(@Query("lat") lat: Double, @Query("lon") lon: Double, @Query("appid") apiKey: String
-    ): Response<WeatherResponse>
+    suspend fun getCurrentWeather(
+        @Query("lat") lat: Double,
+        @Query("lon") lon: Double,
+        @Query("appid") apiKey: String,
+        @Query("units") units: String = "metric"
+
+        ): Response<WeatherResponse>
 
     @GET("forecast")
     suspend fun getWeatherForecast(
-        @Query("lat") lat: Double, @Query("lon") lon: Double, @Query("appid") apiKey: String
-    ):Response<WeatherForecastResponse>
+        @Query("lat") lat: Double,
+        @Query("lon") lon: Double,
+        @Query("appid") apiKey: String,
+        @Query("units") units: String = "metric"
+    ): Response<WeatherForecastResponse>
 }
