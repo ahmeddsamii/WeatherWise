0776169e4aec60472f081a49ff037a5b80afc292
Ahmed Samy <ahmbdelkhalek@gmail.com>
2024-09-28 19:48:22 +0300

    added imageView for icons

--- app/src/main/java/com/example/weatherwise/ui/home/view/HoursAdapter.kt
+++ app/src/main/java/com/example/weatherwise/ui/home/view/HoursAdapter.kt
@@ -5,6 +5,7 @@ import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView.ViewHolder
+import com.example.weatherwise.R
 import com.example.weatherwise.databinding.HourItemBinding
 import com.example.weatherwise.diffUtils.WeatherForecastDiffUtils
 import com.example.weatherwise.model.ListElement
@@ -23,6 +24,41 @@ class HoursAdapter: ListAdapter<ListElement, HoursAdapter.HourViewHolder>(Weathe
     override fun onBindViewHolder(holder: HourViewHolder, position: Int) {
         val currentItem = getItem(position)
         holder.binding.hour.text = currentItem.dtTxt
-        holder.binding.temp.text = currentItem.main.temp.toInt().toString()
+        holder.binding.temp.text = currentItem.main.temp.toInt().toString()+" °C"
+        currentItem.weather.forEach {
+            getWeatherIcon(it.icon)
+            holder.binding.icon.setImageResource(getWeatherIcon(it.icon))
+        }
+
+    }
+
+
+
+
+
+    private fun getWeatherIcon(icon: String): Int {
+        val iconValue: Int
+        when (icon) {
+            "01d" -> iconValue = R.drawable.clear_sky
+            "01n" -> iconValue = R.drawable.clear_sky
+            "02d" -> iconValue = R.drawable.cloudy
+            "02n" -> iconValue = R.drawable.cloudy
+            "03n" -> iconValue = R.drawable.cloudy
+            "03d" -> iconValue = R.drawable.cloudy
+            "04d" -> iconValue = R.drawable.cloudy
+            "04n" -> iconValue = R.drawable.cloudy
+            "09d" -> iconValue = R.drawable.rain
+            "09n" -> iconValue = R.drawable.rain
+            "10d" -> iconValue = R.drawable.rain
+            "10n" -> iconValue = R.drawable.rain
+            "11d" -> iconValue = R.drawable.storm
+            "11n" -> iconValue = R.drawable.storm
+            "13d" -> iconValue = R.drawable.snow
+            "13n" -> iconValue = R.drawable.snow
+            "50d" -> iconValue = R.drawable.mist
+            "50n" -> iconValue = R.drawable.mist
+            else -> iconValue = R.drawable.custom_appbar_shape
+        }
+        return iconValue
     }
 }
