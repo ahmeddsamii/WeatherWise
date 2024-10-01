383b9d098bcbfc258718b80e99b53797b77758a4
Ahmed Samy <ahmbdelkhalek@gmail.com>
2024-10-01 18:00:46 +0300

    added languages parameters to the function

--- app/src/main/java/com/example/weatherwise/network/api/ApiService.kt
+++ app/src/main/java/com/example/weatherwise/network/api/ApiService.kt
@@ -21,6 +21,7 @@ interface ApiService {
         @Query("lat") lat: Double,
         @Query("lon") lon: Double,
         @Query("appid") apiKey: String,
-        @Query("units") units: String = "metric"
+        @Query("units") units: String = "metric",
+        @Query("lang") lang:String
     ): Response<WeatherForecastResponse>
 }
