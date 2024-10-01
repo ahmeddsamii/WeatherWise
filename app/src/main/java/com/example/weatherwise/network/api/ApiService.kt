24024f8e3ee00c219fc2972d54c297f1cd092b5d
Ahmed Samy <ahmbdelkhalek@gmail.com>
2024-10-01 19:37:58 +0300

    added unit parameter

--- app/src/main/java/com/example/weatherwise/network/api/ApiService.kt
+++ app/src/main/java/com/example/weatherwise/network/api/ApiService.kt
@@ -12,7 +12,7 @@ interface ApiService {
         @Query("lat") lat: Double,
         @Query("lon") lon: Double,
         @Query("appid") apiKey: String,
-        @Query("units") units: String = "metric",
+        @Query("units") units: String,
         @Query("lang") lang:String
         ): Response<WeatherResponse>
 
@@ -21,7 +21,7 @@ interface ApiService {
         @Query("lat") lat: Double,
         @Query("lon") lon: Double,
         @Query("appid") apiKey: String,
-        @Query("units") units: String = "metric",
+        @Query("units") units: String,
         @Query("lang") lang:String
     ): Response<WeatherForecastResponse>
 }
