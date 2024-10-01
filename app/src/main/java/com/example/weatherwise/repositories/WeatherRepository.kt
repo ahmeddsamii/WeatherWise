f560441f6e2b5c5d5121e33d743c8d6e380c8809
Ahmed Samy <ahmbdelkhalek@gmail.com>
2024-10-01 18:02:15 +0300

    added language parameters to the functions in the repo

--- app/src/main/java/com/example/weatherwise/repositories/WeatherRepository.kt
+++ app/src/main/java/com/example/weatherwise/repositories/WeatherRepository.kt
@@ -17,11 +17,11 @@ class WeatherRepository private constructor() {
    }
 
 
-    suspend fun getCurrentWeather(lat:Double, long:Double, apiKey:String): Response<WeatherResponse> {
-       return apiService.getCurrentWeather(lat,long,apiKey)
+    suspend fun getCurrentWeather(lat:Double, long:Double, apiKey:String, lang:String): Response<WeatherResponse> {
+       return apiService.getCurrentWeather(lat,long,apiKey, lang = lang)
     }
 
-    suspend fun getWeatherForecast(lat:Double, long:Double, apiKey:String): Response<WeatherForecastResponse>{
-        return apiService.getWeatherForecast(lat,long,apiKey)
+    suspend fun getWeatherForecast(lat:Double, long:Double, apiKey:String, lang:String): Response<WeatherForecastResponse>{
+        return apiService.getWeatherForecast(lat,long,apiKey, lang=lang)
     }
 }
