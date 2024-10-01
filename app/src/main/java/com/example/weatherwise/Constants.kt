1f511fecdedd687daef2ab3a2738495652f265fd
Ahmed Samy <ahmbdelkhalek@gmail.com>
2024-10-01 10:55:07 +0300

    added some constants for map and gps

--- app/src/main/java/com/example/weatherwise/Constants.kt
+++ app/src/main/java/com/example/weatherwise/Constants.kt
@@ -3,6 +3,13 @@ object Constants{
     val LOCATION_REQUEST_CODE = 1001
     const val BASE_URL =  "https://api.openweathermap.org/data/2.5/"
     const val API_KEY = "e9556a170d2136f283a92527fbc67b3d"
+    const val LANGUAGE_SHARED_PREFS = "language sharedPrefs"
     const val LANGUAGE_KEY_SHARED_PREFERENCE = "language"
+    const val COME_FROM_MAP_PREFS = "map activity"
+    const val COME_FROM_MAP_KEY = "ComeFromMapKey"
+    const val MAP_OR_GPS_SHARED_PREFS = "Maps_or_Gps_shared_prefs"
+    const val MAP_OR_GPS_KEY = "map_or_gps"
+    const val LATITUDE = "latitude"
+    const val LONGITUDE = "longitude"
 }
 
