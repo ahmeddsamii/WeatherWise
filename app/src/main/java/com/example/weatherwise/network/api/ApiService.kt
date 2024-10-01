8a8a3a724d97013c3deee038a393a4199c4a26ff
Ahmed Samy <ahmbdelkhalek@gmail.com>
2024-10-01 17:55:32 +0300

    added lang to the end point

--- app/src/main/java/com/example/weatherwise/network/api/ApiService.kt
+++ app/src/main/java/com/example/weatherwise/network/api/ApiService.kt
@@ -12,8 +12,8 @@ interface ApiService {
         @Query("lat") lat: Double,
         @Query("lon") lon: Double,
         @Query("appid") apiKey: String,
-        @Query("units") units: String = "metric"
-
+        @Query("units") units: String = "metric",
+        @Query("lang") lang:String
         ): Response<WeatherResponse>
 
     @GET("forecast")
