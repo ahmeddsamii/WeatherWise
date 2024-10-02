c43fcd49e82792b4859cc94d246a87ee94409bd2
Ahmed Samy <ahmbdelkhalek@gmail.com>
2024-10-02 18:54:01 +0300

    added OnConflictStrategy to prevent exceptions

--- app/src/main/java/com/example/weatherwise/db/PlacesDao.kt
+++ app/src/main/java/com/example/weatherwise/db/PlacesDao.kt
@@ -3,13 +3,14 @@ package com.example.weatherwise.db
 import androidx.room.Dao
 import androidx.room.Delete
 import androidx.room.Insert
+import androidx.room.OnConflictStrategy
 import androidx.room.Query
 import com.example.weatherwise.model.FavoritePlace
 import kotlinx.coroutines.flow.Flow
 
 @Dao
 interface PlacesDao {
-    @Insert
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
     suspend fun addPlace(favoritePlace: FavoritePlace):Long
 
     @Delete
