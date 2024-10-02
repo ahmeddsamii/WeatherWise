3ea1a385b89b82b009bcf39f97e138693dff47a3
Ahmed Samy <ahmbdelkhalek@gmail.com>
2024-10-02 18:51:05 +0300

    added methods of operation add and remove and retreive all favorite places

--- app/src/main/java/com/example/weatherwise/ui/favorite/viewModel/FavoriteViewModel.kt
+++ app/src/main/java/com/example/weatherwise/ui/favorite/viewModel/FavoriteViewModel.kt
@@ -1,13 +1,44 @@
 package com.example.weatherwise.ui.favorite.viewModel
 
+import WeatherRepository
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.weatherwise.model.FavoritePlace
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 
-class FavoriteViewModel : ViewModel() {
+class FavoriteViewModel(val repo:WeatherRepository) : ViewModel() {
 
-    private val _text = MutableLiveData<String>().apply {
-        value = "This is gallery Fragment"
+    private val _addedFavoritePlace = MutableLiveData<FavoritePlace>()
+    val addedFavoritePlace:LiveData<FavoritePlace> = _addedFavoritePlace
+
+    private val _removedFavoritePlace = MutableLiveData<FavoritePlace>()
+    val removedFavoritePlace:LiveData<FavoritePlace> = _removedFavoritePlace
+
+    private val _allLocalFavoritePlaces =  MutableLiveData<List<FavoritePlace>>()
+    val allLocalFavoritePlaces:LiveData<List<FavoritePlace>> = _allLocalFavoritePlaces
+
+
+    fun addFavoritePlace(favoritePlace: FavoritePlace){
+        viewModelScope.launch(Dispatchers.IO) {
+            repo.addPlace(favoritePlace)
+            _addedFavoritePlace.postValue(favoritePlace)
+        }
     }
-    val text: LiveData<String> = _text
+
+    fun removeFavoritePlace(favoritePlace: FavoritePlace){
+        viewModelScope.launch(Dispatchers.IO) {
+            repo.removePlace(favoritePlace)
+            _removedFavoritePlace.postValue(favoritePlace)
+            getAllLocalFavoritePlaces()
+        }
+    }
+
+    suspend fun getAllLocalFavoritePlaces(){
+        repo.getAllLocalFavoritePlaces().collect{
+            _allLocalFavoritePlaces.postValue(it)
+        }
+    }
 }
