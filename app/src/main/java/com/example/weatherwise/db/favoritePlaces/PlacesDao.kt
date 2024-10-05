943eca2ed2fcc36230f42c69ea646510298fd53c
Ahmed Samy <ahmbdelkhalek@gmail.com>
2024-10-05 18:24:08 +0300

    moved from package to package

--- app/src/main/java/com/example/weatherwise/db/favoritePlaces/PlacesDao.kt
+++ app/src/main/java/com/example/weatherwise/db/favoritePlaces/PlacesDao.kt
@@ -1,4 +1,4 @@
-package com.example.weatherwise.db
+package com.example.weatherwise.db.favoritePlaces
 
 import androidx.room.Dao
 import androidx.room.Delete
--- app/src/main/java/com/example/weatherwise/db/favoritePlaces/PlacesLocalDataSource.kt
+++ app/src/main/java/com/example/weatherwise/db/favoritePlaces/PlacesLocalDataSource.kt
@@ -1,7 +1,6 @@
-package com.example.weatherwise.db
+package com.example.weatherwise.db.favoritePlaces
 
 import android.content.Context
-import androidx.room.Dao
 import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
@@ -9,12 +8,12 @@ import com.example.weatherwise.model.FavoritePlace
 
 @Database(entities = [FavoritePlace::class], version = 3)
 abstract class PlacesLocalDataSource:RoomDatabase() {
-    abstract fun PlacesDao():PlacesDao
+    abstract fun PlacesDao(): PlacesDao
 
     companion object{
-        private var INSTANCE:PlacesLocalDataSource? = null
-        fun getInstance(context: Context):PlacesLocalDataSource{
-            return INSTANCE?: synchronized(this){
+        private var INSTANCE: PlacesLocalDataSource? = null
+        fun getInstance(context: Context): PlacesLocalDataSource {
+            return INSTANCE ?: synchronized(this){
                 val instance = Room.databaseBuilder(context, PlacesLocalDataSource::class.java, "placesDB")
                     .fallbackToDestructiveMigration()
                     .build()
