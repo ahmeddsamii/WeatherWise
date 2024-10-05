36f5542c28697caaff8deafd1f83ef7912186261
Ahmed Samy <ahmbdelkhalek@gmail.com>
2024-10-05 18:08:25 +0300

    added alert view model to insert,delete and getAll alerts

--- app/src/main/java/com/example/weatherwise/ui/alert/viewModel/AlertViewModel.kt
+++ app/src/main/java/com/example/weatherwise/ui/alert/viewModel/AlertViewModel.kt
@@ -1,13 +1,69 @@
 package com.example.weatherwise.ui.alert.viewModel
 
+import WeatherRepository
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.weatherwise.model.AlertDto
+import com.example.weatherwise.uiState.UiState
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.catch
+import kotlinx.coroutines.launch
 
-class AlertViewModel : ViewModel() {
+class AlertViewModel(val repository: WeatherRepository) : ViewModel() {
 
-    private val _text = MutableLiveData<String>().apply {
-        value = "This is alert fragment"
+    val _addedAlert = MutableStateFlow<UiState>(UiState.Loading)
+    val addedAlert = _addedAlert.asStateFlow()
+
+
+    val _deletedAlert = MutableStateFlow<UiState>(UiState.Loading)
+    val deletedAlert = _deletedAlert.asStateFlow()
+
+
+    val _allLocalAlerts = MutableStateFlow<UiState>(UiState.Loading)
+    val allLocalAlerts = _allLocalAlerts.asStateFlow()
+
+
+
+
+    fun addAlert(alertDto: AlertDto){
+        viewModelScope.launch (Dispatchers.IO){
+            repository.addAlert(alertDto)
+                .catch {
+                    _addedAlert.value = UiState.Failure(it.message!!)
+                }.collect{
+                    _addedAlert.value = UiState.Success(it)
+                }
+        }
     }
-    val text: LiveData<String> = _text
+
+
+    fun deleteAlert(alertDto: AlertDto){
+        viewModelScope.launch(Dispatchers.IO) {
+            repository.deleteAlert(alertDto)
+                .catch {
+                    _deletedAlert.value = UiState.Failure(it.message!!)
+                }.collect{
+                    _deletedAlert.value = UiState.Success(it)
+                    getAllLocalAlertsByDay()
+                }
+        }
+    }
+
+
+    fun getAllLocalAlertsByDay(){
+        viewModelScope.launch(Dispatchers.IO) {
+            repository.getLocalAlertsByDate()
+                .catch {
+                    _allLocalAlerts.value = UiState.Failure(it.message!!)
+                }.collect{
+                    _allLocalAlerts.value = UiState.Success(it)
+                }
+        }
+    }
+
+
 }
