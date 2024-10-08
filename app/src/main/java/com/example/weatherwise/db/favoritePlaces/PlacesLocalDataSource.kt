f6709bb3520340601ee6dcd7bfe2ce84d5fbe835
Ahmed Samy <ahmbdelkhalek@gmail.com>
2024-10-08 23:18:25 +0300

    added class localDataSource

--- app/src/main/java/com/example/weatherwise/db/favoritePlaces/PlacesLocalDataSource.kt
+++ app/src/main/java/com/example/weatherwise/db/favoritePlaces/PlacesLocalDataSource.kt
@@ -1,25 +1,19 @@
 package com.example.weatherwise.db.favoritePlaces
 
-import android.content.Context
-import androidx.room.Database
-import androidx.room.Room
-import androidx.room.RoomDatabase
 import com.example.weatherwise.model.FavoritePlace
 
-@Database(entities = [FavoritePlace::class], version = 3)
-abstract class PlacesLocalDataSource:RoomDatabase() {
-    abstract fun PlacesDao(): PlacesDao
+class PlacesLocalDataSource(val placesDao: PlacesDao) : IPlacesLocalDataSource {
 
-    companion object{
-        private var INSTANCE: PlacesLocalDataSource? = null
-        fun getInstance(context: Context): PlacesLocalDataSource {
-            return INSTANCE ?: synchronized(this){
-                val instance = Room.databaseBuilder(context, PlacesLocalDataSource::class.java, "placesDB")
-                    .fallbackToDestructiveMigration()
-                    .build()
-                INSTANCE = instance
-                instance
-            }
-        }
+    override suspend fun addPlace(place: FavoritePlace): Long {
+        return placesDao.addPlace(place)
+    }
+
+    override suspend fun removePlace(place: FavoritePlace): Int {
+        return placesDao.deletePlace(place)
+
+    }
+
+    override fun getAllLocalFavoritePlaces(): List<FavoritePlace> {
+        return placesDao.getAllLocalPlaces()
     }
 }
